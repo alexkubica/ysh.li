4d2aef1ed6d3bd2f3dc59ec400edd1eb6198982f
Alex Kubica <me@alexkubica.com>
2024-02-27 20:17:14 +0200

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import Footer from "./components/Footer";
 import LinkButton from "./components/LinkButton";
+import styles from "./page.module.css";
 
 export const metadata: Metadata = {
   title: "יש.לי",
@@ -22,20 +23,30 @@ export default function HomePage() {
           </div>
           <div className="prose text-center">
             <h2>יש.לי</h2>
+          </div>
 
-  <LinkButton url="https://wa.me/972526350655?text=%D7%94%D7%99%D7%99!%20%D7%90%D7%A0%D7%99%20%D7%9E%D7%A2%D7%95%D7%A0%D7%99%D7%99%D7%9F/%D7%AA%20%D7%91%D7%A2%D7%9E%D7%95%D7%93%20%D7%B4%D7%99%D7%A9.%D7%9C%D7%99%D7%B4%20:)">הזמינו עמוד ״יש.לי״ משלכם בחינם לזמן מוגבל!</LinkButton>
+          {/* <span className="countdown font-mono text-2xl">
+            <span className={styles.hour}></span>:
+            <span className={styles.min}></span>:
+            <span className={styles.sec}></span>
+          </span> */}
+          <LinkButton url="https://wa.me/972526350655?text=%D7%94%D7%99%D7%99!%20%D7%90%D7%A0%D7%99%20%D7%9E%D7%A2%D7%95%D7%A0%D7%99%D7%99%D7%9F/%D7%AA%20%D7%91%D7%A2%D7%9E%D7%95%D7%93%20%D7%B4%D7%99%D7%A9.%D7%9C%D7%99%D7%B4%20:)">
+            הזמינו עמוד ״יש.לי״ משלכם בחינם לזמן מוגבל!
+          </LinkButton>
 
-          <LinkButton url="https://t.me/yshlinews">חדשות ישראל בטלגרם</LinkButton>
+          <LinkButton url="https://t.me/yshlinews">
+            חדשות ישראל בטלגרם
+          </LinkButton>
 
-            <h4 className="pt-4">העמודים שלנו</h4>
+          <h4 className="pt-4">העמודים שלנו</h4>
 
           <LinkButton url="/alex">אלכס קוביצה - מייסד יש.לי</LinkButton>
           <LinkButton url="/nextjs">קהילת ה-Next.js של אלכס קוביצה</LinkButton>
           <LinkButton url="/yael">יעל מלינה</LinkButton>
           <LinkButton url="/liam">ליאם</LinkButton>
         </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }
