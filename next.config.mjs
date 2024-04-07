c2d1a129eb9d57f7c1feee708ce3f6e6ea73260e
Alex Kubica <me@alexkubica.com>
2024-04-07 14:52:23 +0300

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   redirects: async () => {
     return [
       {
+        source: "/r",
+        destination:
+          "https://zora.co/collect/base:0x6917816657cee0868d43abe1749b45e3cefa726c/premint-1?referrer=0xd81B7A2a1bBf3e1c713f2A5C886f88EE5f862417",
+        permanent: false,
+        basePath: false,
+      },
+      {
         source: "/a",
         destination: "https://alexkubica.com",
         permanent: false,
