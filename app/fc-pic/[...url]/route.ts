9730c6d856150ffb973de0186cd2dddb71bb9e76
Alex Kubica <me@alexkubica.com>
2024-04-15 15:11:42 +0300

    wip

---
+++ app/fc-pic/[...url]/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+
+type Params = {
+  url: string;
+};
+
+export async function GET(request: Request, context: { params: Params }) {
+  console.log(context);
+  if (context.params.url[0] === "http:" || context.params.url[0] === "https:") {
+    context.params.url[0] = context.params.url[0] + "/";
+  }
+
+  const url = context.params.url.join("/");
+
+  console.log(url);
+
+  return new NextResponse(`
+    <!DOCTYPE html>
+        <html>
+            <head>
+                <meta property="og:image" content="${url}" />
+                <meta property="fc:frame" content="vNext" />
+                <meta property="fc:frame:image" content="${url}" />
+
+                <meta property="fc:frame:button:1" content="ysh.li/fc-pic/IMAGE_URL, made by @alexk" />
+                <meta property="fc:frame:button:1:action" content="link" />
+                <meta property="fc:frame:button:1:target" content="https://alexkubica.com" />
+            </head>
+        </html>
+    `);
+}
