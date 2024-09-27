5ea335c2ed626f268b040811953e7f6a06587e76
Alex Kubica <me@alexkubica.com>
2024-09-27 16:18:03 +0300

    wip

--- app/rozi/page.tsx
+++ app/rozi/page.tsx
@@ -3,6 +3,11 @@ import { Metadata } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { useEffect } from "react";
+import { FaWhatsapp } from "react-icons/fa";
+import { TiSocialFacebook } from "react-icons/ti";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
 
 export default function HomePage() {
   useEffect(() => {
@@ -18,7 +23,7 @@ export default function HomePage() {
         data-theme="rozi"
         className="bg-[#4058DD] font-sans container max-w-screen-md w-full text-center mx-auto p-0 m-0  text-xl md:text-xl font-bold"
       >
-        <div className="mt-8 pt-4 bg-white flex flex-col gap-4  px-4 ">
+        <div className="mt-8 pt-4 bg-white flex flex-col gap-4  px-4 mb-8">
           <div className="p-0 m-0 line-clamp-2 md:line-clamp-1 w-full mx-auto font-extrabold text-xl">
             החלטתם למכור את הבית שלכם, שיהיה בשעה טובה!
           </div>
@@ -51,11 +56,15 @@ export default function HomePage() {
           </ol>
           <div className="text-4xl text-center py-4 border-y-8 border-[#4058DD] mt-16 flex flex-col gap-2">
             <div>חושבים למכור את הבית?</div>
-            <div className="text-[#C9145A] font-extrabold text-4xl">
-              052-895544
-            </div>
+            <a href="tel:+972-52-589-5544">
+              <div className="text-[#C9145A] font-extrabold text-4xl hover:text-red-400">
+                052-895544
+              </div>
+            </a>
             <div>רוזי קוביצה יועצת נדל״ן</div>
-            <div className="text-[#C9145A]">דברו איתי</div>
+            <a href="tel:+972-52-589-5544">
+              <div className="text-[#C9145A] hover:text-red-400">דברו איתי</div>
+            </a>
           </div>
           <div className="flex flex-col md:flex-row gap-4 mt-8  items-center md:px-16 border-b-8 border-[#4058DD] pb-16">
             <img
@@ -63,7 +72,7 @@ export default function HomePage() {
               src="images/rozi/profile-vertical.png"
             />
             <div className="flex flex-col text-right self-start gap-4 px-8">
-              <div className="p-0 m-0 w-full mx-auto text-[#4058DD] font-bold text-4xl text-center md:text-right">
+              <div className="p-0 m-0 w-full mx-auto text-[#4058DD] font-bold text-4xl text-right">
                 מי אני
               </div>
               <div className="text-right text-2xl ">
@@ -108,11 +117,15 @@ export default function HomePage() {
           </div>
           <div className="text-4xl text-center mb-16  border-[#4058DD] mt-16 flex flex-col gap-2">
             <div>חושבים למכור את הבית?</div>
-            <div className="text-[#C9145A] font-extrabold text-4xl">
-              052-895544
-            </div>
+            <a href="tel:+972-52-589-5544">
+              <div className="text-[#C9145A] font-extrabold text-4xl hover:text-red-400">
+                052-895544
+              </div>
+            </a>
             <div>רוזי קוביצה יועצת נדל״ן</div>
-            <div className="text-[#C9145A]">דברו איתי</div>
+            <a href="tel:+972-52-589-5544">
+              <div className="text-[#C9145A] hover:text-red-400">דברו איתי</div>
+            </a>
           </div>
           <div className="flex flex-col gap-8 mb-16 items-center md:px-16 border-t-8 border-[#4058DD] pt-16 text-center text-6xl font-extrabold">
             <div>מה מספרים עלי?</div>
@@ -120,11 +133,15 @@ export default function HomePage() {
           </div>
           <div className="text-4xl text-center py-4 border-y-8 border-[#4058DD] mb-16 flex flex-col gap-2">
             <div>חושבים למכור את הבית?</div>
-            <div className="text-[#C9145A] font-extrabold text-4xl">
-              052-895544
-            </div>
+            <a href="tel:+972-52-589-5544">
+              <div className="text-[#C9145A] font-extrabold text-4xl hover:text-red-400">
+                052-895544
+              </div>
+            </a>
             <div>רוזי קוביצה יועצת נדל״ן</div>
-            <div className="text-[#C9145A]">דברו איתי</div>
+            <a href="tel:+972-52-589-5544">
+              <div className="text-[#C9145A] hover:text-red-400">דברו איתי</div>
+            </a>
           </div>
           <div
             dir="ltr"
@@ -134,6 +151,58 @@ export default function HomePage() {
             <div>&quot;Je parle français.&quot;</div>
             <div className="text-[#4058DD]">&quot;Parlo italiano.&quot;</div>
             <div>&quot;Я говорю по-русски.&quot;</div>
+          </div>
+          <div className="mb-8 text-[#122E76] grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 items-center justify-center gap-y-8">
+            <div className="flex justify-center">
+              <a href="https://api.whatsapp.com/send/?phone=972525895544">
+                <div className="group flex flex-col items-center gap-4 w-fit ">
+                  <div className="size-28 bg-[#38AA4A] group-hover:bg-green-400 rounded-full shadow-xl">
+                    <FaWhatsapp className="w-full h-full p-8 text-white" />
+                  </div>
+                  <div className="group-hover:text-blue-500">ווטסאפ</div>
+                </div>
+              </a>
+            </div>
+            <div className="flex justify-center">
+              <a href="https://www.facebook.com/kubica.rozi">
+                <div className="group flex flex-col items-center gap-4 w-fit">
+                  <div className="size-28 bg-[#4058DD] group-hover:bg-blue-400 rounded-full shadow-xl">
+                    <TiSocialFacebook className="w-full h-full p-8 text-white" />
+                  </div>
+                  <div className="group-hover:text-blue-500">פייסבוק</div>
+                </div>
+              </a>
+            </div>
+            <div className="flex justify-center">
+              <a href="https://www.linkedin.com/in/rozikubica/">
+                <div className="group flex flex-col items-center gap-4 w-fit">
+                  <div className="size-28 bg-[#4058DD] group-hover:bg-blue-400 rounded-full shadow-xl">
+                    <FaLinkedinIn className="w-full h-full p-8 text-white" />
+                  </div>
+                  <div className="group-hover:text-blue-500">לינקדאין</div>
+                </div>
+              </a>
+            </div>
+            <div className="flex justify-center">
+              <a href="https://www.instagram.com/rozi_kubica">
+                <div className="group flex flex-col items-center gap-4 w-fit">
+                  <div className="size-28 bg-[#4058DD] group-hover:bg-blue-400 rounded-full shadow-xl">
+                    <FaInstagram className="w-full h-full p-8 text-white" />
+                  </div>
+                  <div className="group-hover:text-blue-500">אינסטגרם</div>
+                </div>
+              </a>
+            </div>
+            <div className="flex justify-center">
+              <a href="https://www.tiktok.com/@rozi.kubica.realestate">
+                <div className="group flex flex-col items-center gap-4 w-fit">
+                  <div className="size-28 bg-[#4058DD] group-hover:bg-blue-400 rounded-full shadow-xl">
+                    <FaTiktok className="w-full h-full p-8 text-white" />
+                  </div>
+                  <div className="group-hover:text-blue-500">טיקטוק</div>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
